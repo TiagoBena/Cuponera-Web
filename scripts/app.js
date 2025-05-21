08d4b4b367a96afe851613b1fc5043e4b042c36c
TiagoBena <tiagobenavidez@icloud.com>
2025-05-20 21:03:51 -0300

    Delete scripts directory

--- scripts/app.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const main = document.getElementById("main-content");
-    main.innerHTML = "<p>Login y funcionalidades próximamente.</p>";
-});
